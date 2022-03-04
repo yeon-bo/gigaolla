d6d28b7c05675c30ed9360d4b915ea84f367bb19
kimchulyeon <guinness987@gmail.com>
2022-03-04 18:15:36 +0900

    feat: DistributionChart

--- src/components/DistributionChart2.jsx
+++ src/components/DistributionChart2.jsx
@@ -63,7 +63,7 @@ const options = {
 }
 
 // LABEL
-const labels = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, '100 (점)']
+const labels = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, '100  (점)']
 
 //컴포..
 const DistributionChart = () => {
