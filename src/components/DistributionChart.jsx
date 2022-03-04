0b25c096929ea0ac76eaf78de6f4bc4a9fc301c5
kimchulyeon <guinness987@gmail.com>
2022-03-04 23:15:21 +0900

    feat: DistributionChart

--- src/components/DistributionChart.jsx
+++ src/components/DistributionChart.jsx
@@ -64,6 +64,10 @@ const options = {
 
 // LABEL
 const labels = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, '100 (점)']
+// const subjects = {
+//   // 각 과별로 가지고 있는 과목의 종류를 보관하는 전역 변수입니다.
+//   경찰: ['경찰학', '형사법', '헌법'],
+// }
 
 //컴포..
 const DistributionChart = () => {
@@ -71,6 +75,7 @@ const DistributionChart = () => {
 
   const SERIES = params.subject
   const CLASS = params.number
+  // const subject = subjects[SERIES] // 해당하는 과의 과목 목록을 가져옵니다.
 
   const { thisYear, lastMonth } = getLastMonth()
   console.log(thisYear, lastMonth)
@@ -78,6 +83,9 @@ const DistributionChart = () => {
   const [policeSubjectData, setPoliceSubjectData] = useState([])
   const [criminalSubjectData, setCriminalSubjectData] = useState([])
   const [lawSubjectData, setLawSubjectData] = useState([])
+
+  const [data, setData] = useState(null)
+
   useEffect(() => {
     ;(async () => {
       if (!params.number) {
@@ -129,6 +137,44 @@ const DistributionChart = () => {
       }
     })()
   }, [])
+  // useEffect(() => {
+  //   ;(async () => {
+  //     const newData = {} // { [과목명]: 점수 정보 } 를 가지고 있는 임시 변수. 서버에서 가져온 내용을 잠시 보관합니다.
+  //     await Promise.all(
+  //       subject.map(async (i) => {
+  //         // 과목 정보마다 루프를 돕니다.
+  //         const response = await fetch(
+  //           `https://kimcodi.kr/external_api/dashboard/distributionOfStudentScoreByMonth.php?${qs.stringify(
+  //             {
+  //               yyyy: thisYear,
+  //               mm: lastMonth,
+  //               subject: i, // 각 과목
+  //               ...(params.number ? { class: SERIES, classn: CLASS } : null),
+  //             }
+  //           )}`
+  //         )
+  //         newData[i] = (await response.json()).result // newData.과목명에 정보를 저장합니다.
+  //       })
+  //     )
+  //     setData(newData) // 과목명을 모두 불러오면 state를 바꿉니다.
+  //   })().catch(console.error)
+  // })
+  // useEffect(() => {
+  //   ;(async () => {
+  //     const response = await fetch(
+  //       `https://kimcodi.kr/external_api/dashboard/distributionOfStudentScoreByMonth.php?${qs.stringify(
+  //         {
+  //           yyyy: thisYear,
+  //           mm: lastMonth,
+  //           subject: '헌법',
+  //           ...(params.number ? { class: SERIES, classn: CLASS } : null),
+  //         }
+  //       )}`
+  //     )
+  //     const { result } = await response.json()
+  //     setLawSubjectData(result)
+  //   })()
+  // }, [])
 
   //리턴..
   return (
