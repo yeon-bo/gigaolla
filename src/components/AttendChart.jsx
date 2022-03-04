e9762364031fd6341333981bb63775df9e00ea9c
hamdain <dain9812@naver.com>
2022-03-04 21:56:00 +0900

    feat: Add chart tab component

--- src/components/AttendChart.jsx
+++ src/components/AttendChart.jsx
@@ -13,16 +13,13 @@ import {
   Legend,
 } from "chart.js";
 import { Bar, Line } from "react-chartjs-2";
-import DatePicker from "react-datepicker";
-import reset from "../image/reset.svg";
-import calendar from "../image/calendar.svg";
 import { useParams } from "react-router";
+import ChartTab from "./ChartTab";
 
 const AttendChart = () => {
-  const [chartView, setChartView] = useState("bar");
-  const [startDate, setStartDate] = useState(new Date());
   const [totalStudentArr, setTotalStudentArr] = useState([]);
   const [testedStudentArr, setTestedStudentArr] = useState([]);
+  const [chartView, setChartView] = useState("bar");
   const { subject, number } = useParams();
 
   const Cont = styled.div`
@@ -31,45 +28,6 @@ const AttendChart = () => {
     margin: 3em auto;
     canvas {
       max-height: 100% !important;
-    }
-  `;
-  const Tab = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    button {
-      cursor: pointer;
-      padding: 10px 20px;
-      margin-left: 10px;
-      background: #f4f4f4;
-      border-radius: 8px;
-      border: none;
-    }
-    .reset {
-      margin-right: 15px;
-    }
-    .calendarcontainer {
-      position: relative;
-      .datepicker {
-        cursor: pointer;
-        width: 165px;
-        padding: 11px 15px 10px 48px;
-        box-sizing: border-box;
-        background: #f4f4f4;
-        border: none;
-        border-radius: 8px;
-        font-size: 14px;
-        line-height: 19px;
-        text-align: center;
-        &:focus {
-          outline: none;
-        }
-      }
-      img {
-        position: absolute;
-        top: 50%;
-        transform: translateY(-50%);
-        left: 17px;
-      }
     }
   `;
 
@@ -236,21 +194,7 @@ const AttendChart = () => {
 
   return (
     <Cont>
-      <Tab>
-        <button className="reset">
-          <img src={reset} alt="reset" />
-        </button>
-        <div className="calendarcontainer">
-          <img src={calendar} alt="calendar" className="navicon" />
-          <DatePicker
-            className="datepicker"
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-          />
-        </div>
-        <button onClick={() => setChartView("bar")}>비교</button>
-        <button onClick={() => setChartView("chart")}>추이</button>
-      </Tab>
+      <ChartTab onClick={setChartView} />
       {chartView === "bar" ? (
         <Bar options={barOptions} data={chartData} />
       ) : (
