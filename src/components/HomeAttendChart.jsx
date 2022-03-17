46dd982759f4f94eec9f26aa0e66e3d6cd97923c
kimchulyeon <guinness987@gmail.com>
2022-03-17 15:11:46 +0900

    bugFix: resolve conflict & rebase

--- src/components/HomeAttendChart.jsx
+++ src/components/HomeAttendChart.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,35 +9,27 @@ import {
   Title,
   Tooltip,
   Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
-import { logDOM } from "@testing-library/react";
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+// import { logDOM } from "@testing-library/react";
 
 const HomeAttendChart = () => {
-  const [tatalpolice, setTatalpolice] = useState("");
-  const [tatalfire, setTatalfire] = useState("");
-  const [tataladmin, setTataladmin] = useState("");
-  const [testpolice, setTestpolice] = useState("");
-  const [testfire, setTestfire] = useState("");
-  const [testadmin, setTestadmin] = useState("");
+  const [tatalpolice, setTatalpolice] = useState('')
+  const [tatalfire, setTatalfire] = useState('')
+  const [tataladmin, setTataladmin] = useState('')
+  const [testpolice, setTestpolice] = useState('')
+  const [testfire, setTestfire] = useState('')
+  const [testadmin, setTestadmin] = useState('')
 
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
   // OPTION
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: "top",
-        align: "end",
+        position: 'top',
+        align: 'end',
         labels: {
           padding: 10,
           usePointStyle: true,
@@ -48,7 +40,7 @@ const HomeAttendChart = () => {
         },
       },
       tooltop: {
-        backgroundColor: "#5D5FEF",
+        backgroundColor: '#5D5FEF',
       },
     },
     scales: {
@@ -68,151 +60,136 @@ const HomeAttendChart = () => {
         },
       },
     },
-  };
+  }
 
   const getYearMonth = () => {
-    const date = new Date();
-    const nowYear = date.getFullYear();
-    const nowMonth = date.getMonth() + 1;
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let YearMonth = [];
+    const date = new Date()
+    const nowYear = date.getFullYear()
+    const nowMonth = date.getMonth() + 1
+    let year = date.getFullYear()
+    let month = date.getMonth() + 1
+    let YearMonth = []
     for (let i = 0; i <= 5; i++) {
-      YearMonth.unshift({ year, month });
-      month--;
+      YearMonth.unshift({ year, month })
+      month--
       if (month === 0) {
-        month = 12;
-        year = year - 1;
+        month = 12
+        year = year - 1
       }
     }
-    return { nowYear, nowMonth, YearMonth };
-  };
+    return { nowYear, nowMonth, YearMonth }
+  }
 
-  const fetchData = async (
-    year = getYearMonth().nowYear,
-    month = getYearMonth().nowMonth
-  ) => {
-    month = month < 10 ? `0${month}` : month;
-    const URL = "https://kimcodi.kr/external_api/dashboard/";
-    const tatalPoliceUrl = `${URL}numberOfTotalStudentsByMonth.php?yyyy=${year}&mm=${month}&class=경찰`;
-    const tatalFireUrl = `${URL}numberOfTotalStudentsByMonth.php?yyyy=${year}&mm=${month}&class=소방`;
-    const tatalAdminUrl = `${URL}numberOfTotalStudentsByMonth.php?yyyy=${year}&mm=${month}&class=행정`;
-    const testPoliceUrl = `${URL}numberOfTestedStudentsByMonth.php?yyyy=${year}&mm=${month}&class=경찰`;
-    const testFireUrl = `${URL}numberOfTestedStudentsByMonth.php?yyyy=${year}&mm=${month}&class=소방`;
-    const testAdminUrl = `${URL}numberOfTestedStudentsByMonth.php?yyyy=${year}&mm=${month}&class=행정`;
+  const fetchData = async (year = getYearMonth().nowYear, month = getYearMonth().nowMonth) => {
+    month = month < 10 ? `0${month}` : month
+    const URL = 'https://kimcodi.kr/external_api/dashboard/'
+    const tatalPoliceUrl = `${URL}numberOfTotalStudentsByMonth.php?yyyy=${year}&mm=${month}&class=경찰`
+    const tatalFireUrl = `${URL}numberOfTotalStudentsByMonth.php?yyyy=${year}&mm=${month}&class=소방`
+    const tatalAdminUrl = `${URL}numberOfTotalStudentsByMonth.php?yyyy=${year}&mm=${month}&class=행정`
+    const testPoliceUrl = `${URL}numberOfTestedStudentsByMonth.php?yyyy=${year}&mm=${month}&class=경찰`
+    const testFireUrl = `${URL}numberOfTestedStudentsByMonth.php?yyyy=${year}&mm=${month}&class=소방`
+    const testAdminUrl = `${URL}numberOfTestedStudentsByMonth.php?yyyy=${year}&mm=${month}&class=행정`
 
     await Promise.all([
       axios.get(tatalPoliceUrl).then((res) => {
-        if (res.data.code === "001") {
-          setTatalpolice(res.data.result[0].STUDENT_COUNT);
+        if (res.data.code === '001') {
+          setTatalpolice(res.data.result[0].STUDENT_COUNT)
         } else {
-          return;
+          return
         }
       }),
       axios.get(tatalFireUrl).then((res) => {
-        if (res.data.code === "001") {
-          setTatalfire(res.data.result[0].STUDENT_COUNT);
+        if (res.data.code === '001') {
+          setTatalfire(res.data.result[0].STUDENT_COUNT)
         } else {
-          return;
+          return
         }
       }),
       axios.get(tatalAdminUrl).then((res) => {
-        if (res.data.code === "001") {
-          setTataladmin(res.data.result[0].STUDENT_COUNT);
+        if (res.data.code === '001') {
+          setTataladmin(res.data.result[0].STUDENT_COUNT)
         } else {
-          return;
+          return
         }
       }),
       axios.get(testPoliceUrl).then((res) => {
-        if (res.data.code === "001") {
-          setTestpolice(res.data.result[0].STUDENT_COUNT);
+        if (res.data.code === '001') {
+          setTestpolice(res.data.result[0].STUDENT_COUNT)
         } else {
-          return;
+          return
         }
       }),
       axios.get(testFireUrl).then((res) => {
-        if (res.data.code === "001") {
-          setTestfire(res.data.result[0].STUDENT_COUNT);
+        if (res.data.code === '001') {
+          setTestfire(res.data.result[0].STUDENT_COUNT)
         } else {
-          return;
+          return
         }
       }),
       axios.get(testAdminUrl).then((res) => {
-        if (res.data.code === "001") {
-          setTestadmin(res.data.result[0].STUDENT_COUNT);
+        if (res.data.code === '001') {
+          setTestadmin(res.data.result[0].STUDENT_COUNT)
         } else {
-          return;
+          return
         }
       }),
-    ]);
+    ])
 
     //응시율
-    let attendPolice = ((testpolice / tatalpolice) * 100).toFixed(1);
-    attendPolice = attendPolice === "NaN" ? 0 : attendPolice;
-    let attendFire = ((testfire / tatalfire) * 100).toFixed(1);
-    attendFire = attendFire === "NaN" ? 0 : attendFire;
-    let attendAdmin = ((testadmin / tataladmin) * 100).toFixed(1);
-    attendAdmin = attendAdmin === "NaN" ? 0 : attendAdmin;
+    let attendPolice = ((testpolice / tatalpolice) * 100).toFixed(1)
+    attendPolice = attendPolice === 'NaN' ? 0 : attendPolice
+    let attendFire = ((testfire / tatalfire) * 100).toFixed(1)
+    attendFire = attendFire === 'NaN' ? 0 : attendFire
+    let attendAdmin = ((testadmin / tataladmin) * 100).toFixed(1)
+    attendAdmin = attendAdmin === 'NaN' ? 0 : attendAdmin
 
-    return { attendPolice, attendFire, attendAdmin };
-  };
+    return { attendPolice, attendFire, attendAdmin }
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
-  const ym = getYearMonth().YearMonth;
-  const labels = ym.map((label) =>
-    label.month < 10 ? `0${label.month}` : label.month
-  );
-  let policeData = [];
-  let fireData = [];
-  let adminData = [];
+  const ym = getYearMonth().YearMonth
+  const labels = ym.map((label) => (label.month < 10 ? `0${label.month}` : label.month))
+  let policeData = []
+  let fireData = []
+  let adminData = []
 
   ym.map((data) =>
-    fetchData(data.year, data.month).then((res) =>
-      policeData.push(res.attendPolice)
-    )
-  );
-  ym.map((data) =>
-    fetchData(data.year, data.month).then((res) =>
-      fireData.push(res.attendFire)
-    )
-  );
-  ym.map((data) =>
-    fetchData(data.year, data.month).then((res) =>
-      adminData.push(res.attendAdmin)
-    )
-  );
+    fetchData(data.year, data.month).then((res) => policeData.push(res.attendPolice))
+  )
+  ym.map((data) => fetchData(data.year, data.month).then((res) => fireData.push(res.attendFire)))
+  ym.map((data) => fetchData(data.year, data.month).then((res) => adminData.push(res.attendAdmin)))
 
   const chartData = {
     labels,
     datasets: [
       {
-        label: "경찰직",
+        label: '경찰직',
         data: policeData,
-        borderColor: "#21468d",
-        backgroundColor: "#21468d",
+        borderColor: '#21468d',
+        backgroundColor: '#21468d',
         pointBorderWidth: 4,
       },
       {
-        label: "소방직",
+        label: '소방직',
         data: fireData,
-        borderColor: "#fd4f3a",
-        backgroundColor: "#fd4f3a",
+        borderColor: '#fd4f3a',
+        backgroundColor: '#fd4f3a',
         pointBorderWidth: 4,
       },
       {
-        label: "행정직",
+        label: '행정직',
         data: adminData,
-        borderColor: "#257e0e",
-        backgroundColor: "#257e0e",
+        borderColor: '#257e0e',
+        backgroundColor: '#257e0e',
         pointBorderWidth: 4,
       },
     ],
-  };
+  }
 
-  return <Line data={chartData} options={options} />;
-};
+  return <Line data={chartData} options={options} />
+}
 
-export default HomeAttendChart;
+export default HomeAttendChart
