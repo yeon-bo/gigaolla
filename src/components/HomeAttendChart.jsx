65903871230066f6f0d30c54c01a7150e2d4b1b6
kimchulyeon <guinness987@gmail.com>
2022-03-18 01:58:11 +0900

    feat: HomeAttendChart

--- src/components/HomeAttendChart.jsx
+++ src/components/HomeAttendChart.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,57 +9,49 @@ import {
   Title,
   Tooltip,
   Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
-import qs from "qs";
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+import qs from 'qs'
 
-const URL = "https://kimcodi.kr/external_api/dashboard/";
-const TOTAL_Url = `${URL}numberOfTotalStudentsByMonth.php`;
-const TEST_Url = `${URL}numberOfTestedStudentsByMonth.php`;
+const URL = 'https://kimcodi.kr/external_api/dashboard/'
+const TOTAL_Url = `${URL}numberOfTotalStudentsByMonth.php`
+const TEST_Url = `${URL}numberOfTestedStudentsByMonth.php`
 
-ChartJS.register(LineElement);
+ChartJS.register(LineElement)
 
 // 이번 달, 이번 년도 구하기
-let monthArr = [];
-let label = [];
+let monthArr = []
+let label = []
 
 // 월 배열 구하기
 const getMonth = () => {
-  const date = new Date();
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
+  const date = new Date()
+  let year = date.getFullYear()
+  let month = date.getMonth() + 1
 
   for (let i = 1; i <= 6; i++) {
-    month = month < 10 ? `0${month}` : `${month}`;
-    monthArr.unshift({ year, month });
-    label.unshift(month);
-    month--;
+    month = month < 10 ? `0${month}` : `${month}`
+    monthArr.unshift({ year, month })
+    label.unshift(month)
+    month--
     if (month === 0) {
-      month = 12;
-      year = year - 1;
+      month = 12
+      year = year - 1
     }
   }
-  return monthArr;
-};
-getMonth();
+  return monthArr
+}
+getMonth()
 
 const HomeAttendChart = () => {
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
   // OPTION
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: "top",
-        align: "end",
+        position: 'top',
+        align: 'end',
         labels: {
           padding: 10,
           usePointStyle: true,
@@ -70,7 +62,7 @@ const HomeAttendChart = () => {
         },
       },
       tooltop: {
-        backgroundColor: "#5D5FEF",
+        backgroundColor: '#5D5FEF',
       },
     },
     scales: {
@@ -90,14 +82,14 @@ const HomeAttendChart = () => {
         },
       },
     },
-  };
+  }
 
-  const [policeData, setPoliceData] = useState([]);
-  const [fireData, setFireData] = useState([]);
-  const [adminData, setAdminData] = useState([]);
+  const [policeData, setPoliceData] = useState([])
+  const [fireData, setFireData] = useState([])
+  const [adminData, setAdminData] = useState([])
 
   useEffect(() => {
-    (async () => {
+    ;(async () => {
       const subjectData = await Promise.all(
         monthArr.map(async (i) => {
           const resTotal = await fetch(
@@ -105,24 +97,24 @@ const HomeAttendChart = () => {
               yyyy: i.year,
               mm: i.month,
             })}&class=경찰`
-          );
-          const totalData = (await resTotal.json()).result[0].STUDENT_COUNT;
+          )
+          const totalData = (await resTotal.json()).result[0].STUDENT_COUNT
           const resttest = await fetch(
             `${TEST_Url}?${qs.stringify({
               yyyy: i.year,
               mm: i.month,
             })}&class=경찰`
-          );
-          const testData = (await resttest.json()).result[0].STUDENT_COUNT;
-          return Math.round((testData / totalData) * 100);
+          )
+          const testData = (await resttest.json()).result[0].STUDENT_COUNT
+          return Math.round((testData / totalData) * 100)
         })
-      );
-      setPoliceData(subjectData);
-    })().catch(console.error);
-  }, [monthArr]);
+      )
+      setPoliceData(subjectData)
+    })().catch(console.error)
+  }, [monthArr])
 
   useEffect(() => {
-    (async () => {
+    ;(async () => {
       const subjectData = await Promise.all(
         monthArr.map(async (i) => {
           const resTotal = await fetch(
@@ -130,24 +122,24 @@ const HomeAttendChart = () => {
               yyyy: i.year,
               mm: i.month,
             })}&class=소방`
-          );
-          const totalData = (await resTotal.json()).result[0].STUDENT_COUNT;
+          )
+          const totalData = (await resTotal.json()).result[0].STUDENT_COUNT
           const resttest = await fetch(
             `${TEST_Url}?${qs.stringify({
               yyyy: i.year,
               mm: i.month,
             })}&class=소방`
-          );
-          const testData = (await resttest.json()).result[0].STUDENT_COUNT;
-          return Math.round((testData / totalData) * 100);
+          )
+          const testData = (await resttest.json()).result[0].STUDENT_COUNT
+          return Math.round((testData / totalData) * 100)
         })
-      );
-      setFireData(subjectData);
-    })().catch(console.error);
-  }, [monthArr]);
+      )
+      setFireData(subjectData)
+    })().catch(console.error)
+  }, [monthArr])
 
   useEffect(() => {
-    (async () => {
+    ;(async () => {
       const subjectData = await Promise.all(
         monthArr.map(async (i) => {
           const resTotal = await fetch(
@@ -155,48 +147,48 @@ const HomeAttendChart = () => {
               yyyy: i.year,
               mm: i.month,
             })}&class=행정`
-          );
-          const totalData = (await resTotal.json()).result[0].STUDENT_COUNT;
+          )
+          const totalData = (await resTotal.json()).result[0].STUDENT_COUNT
           const resttest = await fetch(
             `${TEST_Url}?${qs.stringify({
               yyyy: i.year,
               mm: i.month,
             })}&class=행정`
-          );
-          const testData = (await resttest.json()).result[0].STUDENT_COUNT;
-          return Math.round((testData / totalData) * 100);
+          )
+          const testData = (await resttest.json()).result[0].STUDENT_COUNT
+          return Math.round((testData / totalData) * 100)
         })
-      );
-      setAdminData(subjectData);
-    })().catch(console.error);
-  }, [monthArr]);
+      )
+      setAdminData(subjectData)
+    })().catch(console.error)
+  }, [monthArr])
 
   const chartData = {
     labels: label,
     datasets: [
       {
-        label: "경찰직",
+        label: '경찰직',
         data: policeData,
-        borderColor: "#21468d",
-        backgroundColor: "#21468d",
+        borderColor: '#21468d',
+        backgroundColor: '#21468d',
         pointBorderWidth: 4,
       },
       {
-        label: "소방직",
+        label: '소방직',
         data: fireData,
-        borderColor: "#fd4f3a",
-        backgroundColor: "#fd4f3a",
+        borderColor: '#fd4f3a',
+        backgroundColor: '#fd4f3a',
         pointBorderWidth: 4,
       },
       {
-        label: "행정직",
+        label: '행정직',
         data: adminData,
-        borderColor: "#257e0e",
-        backgroundColor: "#257e0e",
+        borderColor: '#257e0e',
+        backgroundColor: '#257e0e',
         pointBorderWidth: 4,
       },
     ],
-  };
-  return <Line data={chartData} options={options} />;
-};
-export default HomeAttendChart;
+  }
+  return <Line data={chartData} options={options} />
+}
+export default HomeAttendChart
--- src/pages/Class.jsx
+++ src/pages/Class.jsx
@@ -1,26 +1,22 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 
 // import Navigation from '../components/Navigation'
-import SummaryCard from "../components/SummaryCard";
-import AttendChartWrap from "../components/AttendChartWrap";
-import MaxScoreWrap from "../components/MaxScoreWrap";
-import AverageChartWrap from "../components/AverageChartWrap";
-import DistributionChartWrap from "../components/distribution_chart/DistributionChartWrap";
-import DistributionChartTab from "../components/distribution_chart/DistributionChartTap";
-import SortBySubjectWrap from "../components/distribution_chart/SortBySubjectWrap";
-import { useParams, Link } from "react-router-dom";
-import SummaryCardPercent from "../components/SummaryCardPercent";
-import { getLastMonth } from "../utils/getLastMonth";
+import SummaryCard from '../components/SummaryCard'
+import AttendChartWrap from '../components/AttendChartWrap'
+import MaxScoreWrap from '../components/MaxScoreWrap'
+import AverageChartWrap from '../components/AverageChartWrap'
+import DistributionChartWrap from '../components/distribution_chart/DistributionChartWrap'
+import DistributionChartTab from '../components/distribution_chart/DistributionChartTap'
+import SortBySubjectWrap from '../components/distribution_chart/SortBySubjectWrap'
+import { useParams, Link } from 'react-router-dom'
+import SummaryCardPercent from '../components/SummaryCardPercent'
+import { getLastMonth } from '../utils/getLastMonth'
 
 // 전체 페이지 Wrap
 const Background = styled.div`
   width: 100%;
-<<<<<<< HEAD
-  /* background-color: red; */
-=======
->>>>>>> 75d8b85a2d6ec038d6280b3a191fc183e075a65c
-`;
+`
 // 네비게이션 제외 내용 Wrap
 const Cont = styled.div`
   margin-bottom: 7.63em;
@@ -29,7 +25,7 @@ const Cont = styled.div`
   a {
     text-decoration: none;
   }
-`;
+`
 const PageRound = styled.div`
   position: fixed;
   background: ${(props) => props.theme.navbackgroundColor};
@@ -39,13 +35,13 @@ const PageRound = styled.div`
   left: 19.69em;
   ::after {
     position: fixed;
-    content: "";
+    content: '';
     width: 1.88em;
     height: 100%;
     background: ${(props) => props.theme.mainBackground};
     border-radius: 20px 0 0 20px;
   }
-`;
+`
 // 상단 '00직, 목표 점수 달성도가~' 전체 wrap
 
 // MessageText 가운데 정렬을 위한 wrap
@@ -55,99 +51,99 @@ const Message = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`;
+`
 // '00직, 목표 점수 달성도가~' text
 const MessageText = styled.span`
-  font-family: "Noto Sans KR", sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
   font-weight: bold;
   font-size: 2em;
   color: #fff;
   line-height: 2.75rem;
-`;
+`
 // 재학생, 응시생, 응시율, 상위 카드 wrap
 const SummaryCardCont = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 3.75em;
-`;
+`
 // 차트 wrap
 const ChartCont = styled.div`
   position: relative;
   max-width: 82.5em;
   margin: 0 auto;
   padding-top: 5em;
-`;
+`
 // 차트 Title
 const ChartName = styled.div`
   font-weight: bold;
   font-size: 1.63em;
   line-height: 2.19rem;
   margin-bottom: 3.13rem;
-`;
+`
 const DistributionChartName = styled(ChartName)`
   margin-bottom: 1rem;
-`;
+`
 // 점수지표 차트 wrap
 const Chart2 = styled.div`
   display: flex;
-`;
+`
 
 const Class = () => {
   // 점수대별 인원 전체 / 과목별 버튼
-  const [distributionTotal, setDistributionTotal] = useState(true);
-  const [achieveStudent, setAchieveStudent] = useState(0);
-  const [achieveCompareStudent, setAchieveCompareStudent] = useState(0);
-  const [achieveIncrement, setAchieveIncrement] = useState(0);
-  const { subject, number } = useParams();
-  const { thisYear, lastMonth } = getLastMonth();
+  const [distributionTotal, setDistributionTotal] = useState(true)
+  const [achieveStudent, setAchieveStudent] = useState(0)
+  const [achieveCompareStudent, setAchieveCompareStudent] = useState(0)
+  const [achieveIncrement, setAchieveIncrement] = useState(0)
+  const { subject, number } = useParams()
+  const { thisYear, lastMonth } = getLastMonth()
 
   useEffect(() => {
-    let compareMonth = Number(lastMonth) === 1 ? 12 : Number(lastMonth) - 1;
-    compareMonth = compareMonth < 10 ? `0${compareMonth}` : compareMonth;
-    let compareYear = compareMonth === 12 ? thisYear - 1 : thisYear;
-    const URL = `https://kimcodi.kr/external_api/dashboard/studentInfoOfClassByMonth.php?yyyy=${thisYear}&mm=${lastMonth}&class=${subject}`;
-    const COMPARE_URL = `https://kimcodi.kr/external_api/dashboard/studentInfoOfClassByMonth.php?yyyy=${compareYear}&mm=${compareMonth}&class=${subject}`;
-    let fetchUrl = "";
-    let fetchCompareUrl = "";
+    let compareMonth = Number(lastMonth) === 1 ? 12 : Number(lastMonth) - 1
+    compareMonth = compareMonth < 10 ? `0${compareMonth}` : compareMonth
+    let compareYear = compareMonth === 12 ? thisYear - 1 : thisYear
+    const URL = `https://kimcodi.kr/external_api/dashboard/studentInfoOfClassByMonth.php?yyyy=${thisYear}&mm=${lastMonth}&class=${subject}`
+    const COMPARE_URL = `https://kimcodi.kr/external_api/dashboard/studentInfoOfClassByMonth.php?yyyy=${compareYear}&mm=${compareMonth}&class=${subject}`
+    let fetchUrl = ''
+    let fetchCompareUrl = ''
     if (!number) {
-      fetchUrl = URL;
-      fetchCompareUrl = COMPARE_URL;
+      fetchUrl = URL
+      fetchCompareUrl = COMPARE_URL
     } else {
-      fetchUrl = `${URL}&classn=${number}`;
-      fetchCompareUrl = `${COMPARE_URL}&classn=${number}`;
+      fetchUrl = `${URL}&classn=${number}`
+      fetchCompareUrl = `${COMPARE_URL}&classn=${number}`
     }
     const fetchData = async () => {
-      const response = await fetch(fetchUrl);
-      const { result } = await response.json();
-      const achieveCount = [];
+      const response = await fetch(fetchUrl)
+      const { result } = await response.json()
+      const achieveCount = []
       result.map((student) => {
-        let achieve = Number(student.달성도.split("%")[0]);
+        let achieve = Number(student.달성도.split('%')[0])
         if (achieve >= 90) {
-          achieveCount.push(achieve);
+          achieveCount.push(achieve)
         }
-      });
-      setAchieveStudent(achieveCount.length);
-      return;
-    };
+      })
+      setAchieveStudent(achieveCount.length)
+      return
+    }
     const fetchCompareData = async () => {
-      const response = await fetch(fetchCompareUrl);
-      const { result } = await response.json();
-      const achieveCount = [];
+      const response = await fetch(fetchCompareUrl)
+      const { result } = await response.json()
+      const achieveCount = []
       result.map((student) => {
-        let achieve = Number(student.달성도.split("%")[0]);
+        let achieve = Number(student.달성도.split('%')[0])
         if (achieve >= 90) {
-          achieveCount.push(achieve);
+          achieveCount.push(achieve)
         }
-      });
-      setAchieveCompareStudent(achieveCount.length);
-      const increment = await (achieveStudent - achieveCompareStudent);
-      setAchieveIncrement(increment);
-      return;
-    };
+      })
+      setAchieveCompareStudent(achieveCount.length)
+      const increment = await (achieveStudent - achieveCompareStudent)
+      setAchieveIncrement(increment)
+      return
+    }
 
-    fetchData();
-    fetchCompareData();
+    fetchData()
+    fetchCompareData()
   }, [
     subject,
     number,
@@ -156,21 +152,21 @@ const Class = () => {
     achieveStudent,
     achieveCompareStudent,
     achieveIncrement,
-  ]);
+  ])
 
-  let subjectColor;
+  let subjectColor
   switch (subject) {
-    case "경찰":
-      subjectColor = "#21468d";
-      break;
-    case "소방":
-      subjectColor = "#fd4f3a";
-      break;
-    case "행정":
-      subjectColor = "#257e0e";
-      break;
+    case '경찰':
+      subjectColor = '#21468d'
+      break
+    case '소방':
+      subjectColor = '#fd4f3a'
+      break
+    case '행정':
+      subjectColor = '#257e0e'
+      break
     default:
-      subjectColor = "#5d5fef";
+      subjectColor = '#5d5fef'
   }
 
   const MessageCont = styled.div`
@@ -180,7 +176,7 @@ const Class = () => {
     background: ${(props) => props.subjectColor};
     /* background-color: #21468d; */
     border-radius: 16px;
-  `;
+  `
 
   // 상단 공지 색깔 변경
   return (
@@ -192,27 +188,26 @@ const Class = () => {
           {/* <MessageCont> */}
           <Message>
             <MessageText>
-              {subject}직 {number ? `${number}반` : ""}, 목표 점수 달성도가
-              90%가 넘은 학생은 {achieveStudent}명이며 저번 시험 대비{" "}
-              {achieveIncrement}명 올랐습니다.
+              {subject}직 {number ? `${number}반` : ''}, 목표 점수 달성도가 90%가 넘은 학생은{' '}
+              {achieveStudent}명이며 저번 시험 대비 {achieveIncrement}명 올랐습니다.
             </MessageText>
           </Message>
         </MessageCont>
         {number ? (
           <Link to={`/${subject}/${number}/students`}>
             <SummaryCardCont>
-              <SummaryCard Title={"재학생"} Subject={subject} Number={number} />
-              <SummaryCard Title={"응시생"} Subject={subject} Number={number} />
-              <SummaryCard Title={"응시율"} Subject={subject} Number={number} />
-              <SummaryCard Title={"반등수"} Subject={subject} Number={number} />
+              <SummaryCard Title={'재학생'} Subject={subject} Number={number} />
+              <SummaryCard Title={'응시생'} Subject={subject} Number={number} />
+              <SummaryCard Title={'응시율'} Subject={subject} Number={number} />
+              <SummaryCard Title={'반등수'} Subject={subject} Number={number} />
             </SummaryCardCont>
           </Link>
         ) : (
           <Link to={`/${subject}/students`}>
             <SummaryCardCont>
-              <SummaryCard Title={"재학생"} Subject={subject} />
-              <SummaryCard Title={"응시생"} Subject={subject} />
-              <SummaryCard Title={"응시율"} Subject={subject} />
+              <SummaryCard Title={'재학생'} Subject={subject} />
+              <SummaryCard Title={'응시생'} Subject={subject} />
+              <SummaryCard Title={'응시율'} Subject={subject} />
               <SummaryCardPercent Subject={subject} />
             </SummaryCardCont>
           </Link>
@@ -233,15 +228,11 @@ const Class = () => {
             <span>점수대별 인원수</span>
             <DistributionChartTab setDistributionTotal={setDistributionTotal} />
           </DistributionChartName>
-          {distributionTotal ? (
-            <DistributionChartWrap />
-          ) : (
-            <SortBySubjectWrap />
-          )}
+          {distributionTotal ? <DistributionChartWrap /> : <SortBySubjectWrap />}
         </ChartCont>
       </Cont>
     </Background>
-  );
-};
+  )
+}
 
-export default Class;
+export default Class
