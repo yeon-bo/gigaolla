8ea3497af1d34c14916380c2720b124584e02f75
hamdain <dain9812@naver.com>
2022-03-17 15:55:21 +0900

    feat: Add student list search input

--- src/components/student/StudentListTable.jsx
+++ src/components/student/StudentListTable.jsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react";
 import StudentListItem from "./StudentListItem";
-import { useTable, useSortBy } from "react-table";
+import { useTable, useSortBy, useFilters } from "react-table";
 import styled from "styled-components";
 
 const Tr = styled.tr`
@@ -15,6 +15,48 @@ const Tr = styled.tr`
       #ffffff;
   }
 `;
+const Input = styled.input`
+  display: block;
+  box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.1);
+  border-radius: 15px;
+  border: none;
+  width: 95%;
+  margin: 0 auto;
+  height: 48px;
+  padding-left: 12px;
+  box-sizing: border-box;
+`;
+
+function SelectColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set();
+    preFilteredRows.forEach((row) => {
+      options.add(row.values[id]);
+    });
+    return [...options.values()];
+  }, [id, preFilteredRows]);
+
+  // Render a multi-select box
+  return (
+    <select
+      value={filterValue}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined);
+      }}
+    >
+      <option value="">All</option>
+      {options.map((option, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
 
 // Table Title Row
 const COLUMNS = [
@@ -55,52 +97,66 @@ const StudentListTable = ({
       columns,
       data,
     },
+    useFilters,
     useSortBy
   );
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setFilter,
+  } = tableInstance;
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => {
-          return (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => {
-                return (
-                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                    {column.render("Header")}
-                    {/* <span>{column.isSorted ? (column.isSortedDesc ? ' 🔽 ' : ' 🔼 ') : ''}</span> */}
-                  </th>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <Tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                // console.log(cell.row.original)
-                return (
-                  <td
-                    onClick={() => {
-                      getStudentDetailInfo(cell.row.original);
-                      setCanBringData(true);
-                    }}
-                    {...cell.getCellProps()}
-                  >
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
-            </Tr>
-          );
-        })}
-        {/* {mockData.map((data, idx) => {
+    <>
+      <Input
+        placeholder="이름을 입력하세요"
+        onChange={(e) => setFilter("회원명", e.target.value)}
+      />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => {
+            return (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => {
+                  return (
+                    <th
+                      {...column.getHeaderProps(column.getSortByToggleProps())}
+                    >
+                      {column.render("Header")}
+                      {/* <span>{column.isSorted ? (column.isSortedDesc ? ' 🔽 ' : ' 🔼 ') : ''}</span> */}
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <Tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  // console.log(cell.row.original)
+                  return (
+                    <td
+                      onClick={() => {
+                        getStudentDetailInfo(cell.row.original);
+                        setCanBringData(true);
+                      }}
+                      {...cell.getCellProps()}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+          {/* {mockData.map((data, idx) => {
           return (
             <StudentListItem
               key={idx}
@@ -110,8 +166,9 @@ const StudentListTable = ({
             />
           )
         })} */}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </>
   );
 
   // const tableInstance = useTable(
